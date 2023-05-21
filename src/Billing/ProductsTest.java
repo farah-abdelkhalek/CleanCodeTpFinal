a8ed81b19b5afccaa2656fed1a3a0c252fac3011
farah-abdelkhalek <farah.abdelkhalek15@gmail.com>
2023-05-21 23:34:01 +0200

    creation of ProductsTest

---
+++ src/Billing/ProductsTest.java
@@ -0,0 +1,19 @@
+package Billing;
+
+public class ProductsTest {
+    public static void main(String[] args) {
+        Products product = new Products("perfume", 27.99, true, "Cosmetic");
+
+        String name = product.getName();
+        double price = product.getPrice();
+        String category = product.getCategory();
+        boolean importStatus = product.isImported();
+
+        System.out.println("Product Information:");
+        System.out.println("Name: " + name);
+        System.out.println("Price: €" + price);
+        System.out.println("Category: " + category);
+        System.out.println("Import Status: " + (importStatus ? "Imported" : "Not Imported"));
+    }
+}
+
---
+++ src/Billing/TaxSystem.java
@@ -0,0 +1,5 @@
+package Billing;
+
+public class TaxSystem {
+
+}
