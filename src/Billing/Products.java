8ed61c6c3c820c0d591494c441e661bde4b69acf
farah-abdelkhalek <farah.abdelkhalek15@gmail.com>
2023-05-21 23:26:00 +0200

    Products class creation

---
+++ src/Billing/Products.java
@@ -0,0 +1,33 @@
+package Billing;
+
+public class Products {
+
+    private final String name;
+    private final double price;
+    private final String category;
+    private final boolean importStatus;
+
+    public Products(String name, double price, boolean importStatus, String category) {
+        this.name = name;
+        this.price = price;
+        this.importStatus = importStatus;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isImported() {
+        return importStatus;
+    }
+}
+
