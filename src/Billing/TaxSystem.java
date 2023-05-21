7162108e0c35febdb4da8c9b9bc575028aac3a67
farah-abdelkhalek <farah.abdelkhalek15@gmail.com>
2023-05-22 00:13:43 +0200

    creation of TaxSystem

--- src/Billing/TaxSystem.java
+++ src/Billing/TaxSystem.java
@@ -1,5 +1,35 @@
 package Billing;
 
-public class TaxSystem {
+public enum TaxSystem {
+    TAX {
+        public double calculateTax(Products product) {
+            return product.getPrice() * 0.1;
+            }
+        },
+    NO_TAX {
+        public double calculateTax(Products product) {
+            return 0;
+            }
+        },
+    IMPORTED_TAX {
+        public double calculateTax(Products product) {
+            if (product.isImported()) {
+                return product.getPrice() * 0.15;
+            } else {
+                return 0;
+                }
+            }
+        },
+    IMPORTED_NO_TAX {
+        public double calculateTax(Products product) {
+            if (product.isImported()) {
+                return product.getPrice() * 0.05;
+            } else {
+                return 0;
+                }
+            }
+        };
 
+    public abstract double calculateTax(Products product);
 }
+
