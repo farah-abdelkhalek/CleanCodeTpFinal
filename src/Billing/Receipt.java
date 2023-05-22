d85a19940271f690854bdcd9e7812de24f1e2454
farah-abdelkhalek <farah.abdelkhalek15@gmail.com>
2023-05-22 13:02:13 +0200

    Additional work on TaxCalculationTest and creation of Receipt class

---
+++ src/Billing/Receipt.java
@@ -0,0 +1,45 @@
+package Billing;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Receipt {
+    private List<Products> listOfProducts;
+    private Map<Products, Integer> productCountMap;
+    private double totalTax;
+    private double totalPriceWithTax;
+
+    public Receipt(List<Products> listOfProducts, double totalTax, double totalPriceWithTax) {
+        this.listOfProducts = listOfProducts;
+        this.totalTax = totalTax;
+        this.totalPriceWithTax = totalPriceWithTax;
+        this.productCountMap = calculateProductCountMap(listOfProducts);
+    }
+
+    private Map<Products, Integer> calculateProductCountMap(List<Products> productsList) {
+        Map<Products, Integer> countMap = new HashMap<>();
+
+        for (Products product : productsList) {
+            countMap.put(product, countMap.getOrDefault(product, 0) + 1);
+        }
+
+        return countMap;
+    }
+
+    public List<Products> getListOfProducts() {
+        return listOfProducts;
+    }
+
+    public Map<Products, Integer> getProductCountMap() {
+        return productCountMap;
+    }
+
+    public double getTotalTax() {
+        return totalTax;
+    }
+
+    public double getTotalPriceWithTax() {
+        return totalPriceWithTax;
+    }
+}
---
+++ src/Billing/TaxCalculationTest.java
@@ -0,0 +1,31 @@
+package Billing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaxCalculationTest {
+    public static void main(String[] args) {
+        List<Products> productsList = new ArrayList<>();
+        productsList.add(new Products("Imported bottle of perfume", 27.99, true, "Cosmetic"));
+        productsList.add(new Products("Bottle of perfume", 18.99, false, "Cosmetic"));
+        productsList.add(new Products("Box of migraine medication", 9.75, false, "Medicine"));
+        productsList.add(new Products("Box of imported chocolates", 11.25, true, "Food"));
+
+        TaxCalculation taxCalculation = new TaxCalculation();
+
+        double totalTaxAmount = taxCalculation.calculateTaxAmount(productsList);
+
+        System.out.println("Tax amount :"+ totalTaxAmount, "€");
+        System.out.println("Total:" + calculateTotalAmount(productsList, totalTaxAmount), "€");
+    }
+
+    private static double calculateTotalAmount(List<Products> productsList, double totalTaxAmount) {
+        double totalAmount = 0;
+
+        for (Products product : productsList) {
+            totalAmount += product.getPrice();
+        }
+
+        return totalAmount + totalTaxAmount;
+    }
+}
